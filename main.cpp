4f337e434b014983e9673c6d7a7087e695a5ec09
Andrew Godfrey <agodfrey3@fordham.edu>
2016-11-26 22:37:15 -0500

    added default maze option and improved display function

--- main.cpp
+++ main.cpp
@@ -15,6 +15,15 @@ public:
 	{
 		head = NULL;
 	}
+	~Stack()
+	{
+		while (head)
+			{
+				Node * tmp = head;
+				head = head->next;
+				delete tmp;
+		}
+	}
 	void push(Type data)
 	{
 		if (head)
@@ -66,7 +75,7 @@ public:
 	}
 	bool operator==(const Point & rhs)
 	{
-		return (x == rhs.x && y == rhs.y)? true:false;
+		return (x == rhs.x && y == rhs.y) ? true : false;
 	}
 	void print()
 	{
@@ -74,7 +83,7 @@ public:
 	}
 	Point right()
 	{
-		Point tmp(x, y+1);
+		Point tmp(x, y + 1);
 		return tmp;
 	}
 	Point left()
@@ -84,12 +93,12 @@ public:
 	}
 	Point up()
 	{
-		Point tmp(x-1, y);
+		Point tmp(x - 1, y);
 		return tmp;
 	}
 	Point down()
 	{
-		Point tmp(x+1, y);
+		Point tmp(x + 1, y);
 		return tmp;
 	}
 	int getX()
@@ -106,33 +115,23 @@ private:
 
 class Maze {
 public:
-	Maze(string file)
+	Maze()
 	{
-		pathLen = 0;
-		int row = 0, col = 0;
-		ifstream infile(file);
-		if (infile.is_open())
+		cout << "Would you like to load a maze from a file, or use the default maze?\n"
+			<< "1. Default\n"
+			<< "2. Load from file\n";
+		int choice;
+		cin >> choice;
+		if (choice == 2)
 		{
-			infile >> height >> width;
-			grid = new char*[height];
-			visited = new char*[height];
-			for (int i = 0; i < height; i++)
-			{
-				grid[i] = new char[width];
-				visited[i] = new char[width];
-			}
-			for (int row = 0; row < height; row++) 
-			{
-				for (int col = 0; col < width; col++)
-				{
-					infile >> grid[row][col];
-					visited[row][col];
-				}	
-			}
-			infile.close();
+			string file;
+			cout << "Enter the file path: ";
+			cin >> file;
+			loadFromFile(file);
 		}
 		else
-			cout << "File not found!\n";
+			loadDefault();
+		printMaze();
 	}
 	~Maze()
 	{
@@ -144,15 +143,77 @@ public:
 		delete grid;
 		delete visited;
 	}
+	void loadFromFile(string file)
+	{
+		int row = 0, col = 0;
+		ifstream infile(file);
+		if (infile.is_open())
+		{
+			infile >> height >> width;
+			grid = new char*[height];
+			visited = new char*[height];
+			for (int row = 0; row < height; row++)
+			{
+				grid[row] = new char[width];
+				visited[row] = new char[width];
+				for (int col = 0; col < width; col++)
+				{
+					infile >> grid[row][col];
+					visited[row][col];
+				}
+			}
+			infile.close();		}
+		else
+		{
+			cout << "File not found! Please enter the file again! : ";
+			string file;
+			cin >> file;
+			loadFromFile(file);
+		}
+	}
+	void loadDefault()
+	{
+		height = 5; 
+		width = 5;
+		grid = new char*[height];
+		visited = new char*[height];
+		string a[5] = { "XOXEX", 
+					    "OOOOO", 
+			            "XXOXX", 
+			            "OOOOX", 
+			            "XXOXX" };
+		for (int i = 0; i < height; i++)
+		{
+			grid[i] = new char[width];
+			visited[i] = new char[width];
+			for (int j = 0; j < width; j++)
+			{
+				grid[i][j] = a[i][j];
+				visited[i][j] = 'O';
+			}
+		}
+		initSuccess = true;
+	}
 	void printMaze()
 	{
 		cout << "-Maze-\n";
-		for(int i = 0; i < height; i++)
+		for (int i = 0; i < height; i++)
 		{
 			for (int j = 0; j < width; j++)
 				cout << grid[i][j];
 			cout << endl;
 		}
+	}
+	void displayResult(Point test)
+	{
+		if (canGetOut(test))
+		{
+			cout << "There is a way out!\n"
+				<< "One path is: \n";
+			showPath();
+		}
+		else
+			cout << "There is no way out!\n";
 	}
 	bool canGetOut(Point curr)
 	{
@@ -179,7 +240,7 @@ public:
 			path.push("left\n");
 			return true;
 		}
-		if(!path.isEmpty())
+		if (!path.isEmpty())
 			path.pop();
 		return false;
 	}
@@ -195,6 +256,7 @@ public:
 			cout << path.top();
 			path.pop();
 		}
+		cout << endl;
 	}
 	bool isVisited(Point curr)
 	{
@@ -210,20 +272,21 @@ public:
 			return false;
 		return true;
 	}
+	bool isGood()
+	{
+		return initSuccess;
+	}
 private:
 	int height, width;
 	char ** grid;
 	char ** visited;
 	Stack<string>path;
-	int pathLen;
+	bool initSuccess;
 };
 int main()
 {
-	Maze test("C:\\Users\\Andrew\\Desktop/maze.txt");
-	test.printMaze();
+	Maze test;
 	Point testPoint(0, 1);
-	string result = test.canGetOut(testPoint) ? "Yes\n" : "No\n";
-	cout << result;
-	test.showPath();
+	test.displayResult(testPoint);
 	return 0;
 }
